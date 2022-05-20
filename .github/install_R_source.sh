456dd2602621c5a8433782466f76f4b9d159bcf8
ElinorK <16242091+elinork@users.noreply.github.com>
2022-05-20 07:42:38 -0400

    adding back

--- .github/install_R_source.sh
+++ .github/install_R_source.sh
@@ -31,6 +31,68 @@ if [ "${UBUNTU_VERSION}" == "bionic" ]; then
     READLINE_VERSION=7
 fi
 
+apt-get install -y --no-install-recommends \
+    bash-completion \
+    ca-certificates \
+    devscripts \
+    file \
+    fonts-texgyre \
+    g++ \
+    gfortran \
+    gsfonts \
+    libblas-dev \
+    libbz2-* \
+    libcurl4 \
+    libicu* \
+    liblapack-dev \
+    libpcre2* \
+    libjpeg-turbo* \
+    libpangocairo-* \
+    libpng16* \
+    "libreadline${READLINE_VERSION}" \
+    libtiff* \
+    liblzma* \
+    make \
+    unzip \
+    zip \
+    zlib1g
+
+BUILDDEPS="curl \
+    default-jdk \
+    libbz2-dev \
+    libcairo2-dev \
+    libcurl4-openssl-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libicu-dev \
+    libpcre2-dev \
+    libpng-dev \
+    libreadline-dev \
+    libtiff5-dev \
+    liblzma-dev \
+    libx11-dev \
+    libxt-dev \
+    perl \
+    rsync \
+    subversion \
+    tcl-dev \
+    tk-dev \
+    texinfo \
+    texlive-extra-utils \
+    texlive-fonts-recommended \
+    texlive-fonts-extra \
+    texlive-latex-recommended \
+    texlive-latex-extra \
+    x11proto-core-dev \
+    xauth \
+    xfonts-base \
+    xvfb \
+    wget \
+    zlib1g-dev"
+
+# shellcheck disable=SC2086
+apt-get install -y --no-install-recommends ${BUILDDEPS}
+
 ## Download R from 0-Cloud CRAN mirror or CRAN
 function download_r_src() {
     wget "https://cloud.r-project.org/src/$1" -O "R.tar.gz" ||
